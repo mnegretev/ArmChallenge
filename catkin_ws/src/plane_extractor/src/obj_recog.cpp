4dfc842be839d13e97b9ecd667ea588c7df2a3f0
Mark The Magnificent <marco.negrete@ingenieria.unam.edu>
2022-04-27 20:41:31 -0500

    Add a draft for clusterizing objects

--- catkin_ws/src/plane_extractor/src/obj_recog.cpp
+++ catkin_ws/src/plane_extractor/src/obj_recog.cpp
@@ -3,6 +3,7 @@
 #include "std_msgs/Empty.h"
 #include "sensor_msgs/PointCloud2.h"
 #include "sensor_msgs/Image.h"
+#include "visualization_msgs/Marker.h"
 #include "tf/transform_listener.h"
 #include "pcl_ros/transforms.h"
 
@@ -29,22 +30,8 @@ void PointCloud2MsgToCvMat(sensor_msgs::PointCloud2& pc_msg, cv::Mat& bgr_dest, 
         }
 }
 
-
-void callback_point_cloud(const sensor_msgs::PointCloud2::ConstPtr& msg)
+cv::Mat get_image_no_background(cv::Mat bgr, cv::Mat xyz_camera, cv::Mat xyz_robot)
 {
-    sensor_msgs::PointCloud2 cloud_camera = *msg;
-    sensor_msgs::PointCloud2 cloud_robot;
-    pcl_ros::transformPointCloud("world", cloud_camera, cloud_robot, *tf_listener);
-    cv::Mat bgr;
-    cv::Mat xyz_robot;
-    cv::Mat xyz_camera;
-    PointCloud2MsgToCvMat(cloud_camera, bgr, xyz_camera);
-    PointCloud2MsgToCvMat(cloud_robot , bgr, xyz_robot );
-    if(bgr.cols < 1 || bgr.rows < 1)
-    {
-        std::cout << "Invalid point cloud ..." << std::endl;
-        return;
-    }
     for(int i=0; i < bgr.rows; i++)
         for(int j=0; j < bgr.cols; j++)
         {
@@ -55,6 +42,63 @@ void callback_point_cloud(const sensor_msgs::PointCloud2::ConstPtr& msg)
                 bgr.at<cv::Vec3b>(i,j) = cv::Vec3b(0,0,0);
             }
         }
+    return bgr;
+}
+
+cv::Mat color_segmentation(cv::Mat bgr)
+{
+    cv::Mat hsv;
+    cv::cvtColor(bgr, hsv, CV_BGR2HSV);
+    cv::Mat bin_b, bin_g, bin_r, bin_y, bin_objects;
+    cv::inRange(hsv, cv::Scalar(110, 200, 30), cv::Scalar(130, 255, 255), bin_b);
+    cv::inRange(hsv, cv::Scalar( 50, 200, 30), cv::Scalar( 70, 255, 255), bin_g);
+    cv::inRange(hsv, cv::Scalar(  0, 200, 30), cv::Scalar( 10, 255, 255), bin_r);
+    cv::inRange(hsv, cv::Scalar( 20, 200, 30), cv::Scalar( 40, 255, 255), bin_y);
+    cv::bitwise_or(bin_b, bin_g, bin_objects);
+    cv::bitwise_or(bin_objects, bin_r, bin_objects);
+    cv::bitwise_or(bin_objects, bin_y, bin_objects);
+    cv::imshow("Objects", bin_objects);
+    return bin_objects;
+}
+
+cv::Mat clusterize(cv::Mat bin, cv::Mat xyz_robot)
+{
+    std::vector<cv::Vec3f> filtered;
+    cv::Mat labels, centers;
+    for(int i=0; i < bin.rows; i++)
+        for(int j=0; j < bin.cols; j++)
+            if(bin.at<unsigned char>(i,j) != 0)
+                filtered.push_back(xyz_robot.at<cv::Vec3f>(i,j));
+    cv::kmeans(filtered, 5, labels, cv::TermCriteria( cv::TermCriteria::EPS + cv::TermCriteria::COUNT, 10, 1.0),
+               3, cv::KMEANS_PP_CENTERS, centers);
+    //std::cout << "Found " << centers.rows<< " centroids from  " << filtered.size() << " points" << std::endl;
+    return centers;
+}
+
+visualization_msgs::Marker get_object_marker(cv::Mat centroids)
+{
+    visualization_msgs::Marker mrk;
+    
+}
+
+void callback_point_cloud(const sensor_msgs::PointCloud2::ConstPtr& msg)
+{
+    sensor_msgs::PointCloud2 cloud_camera = *msg;
+    sensor_msgs::PointCloud2 cloud_robot;
+    pcl_ros::transformPointCloud("world", cloud_camera, cloud_robot, *tf_listener);
+    cv::Mat bgr, bin, centroids;
+    cv::Mat xyz_robot;
+    cv::Mat xyz_camera;
+    PointCloud2MsgToCvMat(cloud_camera, bgr, xyz_camera);
+    PointCloud2MsgToCvMat(cloud_robot , bgr, xyz_robot );
+    if(bgr.cols < 1 || bgr.rows < 1)
+    {
+        std::cout << "Invalid point cloud ..." << std::endl;
+        return;
+    }
+    bgr = get_image_no_background(bgr, xyz_camera, xyz_robot);
+    bin = color_segmentation(bgr);
+    centroids = clusterize(bin, xyz_robot);
     cv::imshow("Extracted Plane", bgr);
 }
 
@@ -63,8 +107,8 @@ void on_d_changed(int, void*){}
 
 int main(int argc, char** argv)
 {
-    std::cout << "INITIALIZING PLANE EXTRACTOR BY MARCOSOFT ..." << std::endl;
-    ros::init(argc, argv, "cloud_capturer");
+    std::cout << "INITIALIZING OBJECT RECOGNIZER BY MARCOSOFT ..." << std::endl;
+    ros::init(argc, argv, "obj_recog");
     ros::NodeHandle n;
     ros::Subscriber sub_cloud = n.subscribe("/camera/depth/points", 1, callback_point_cloud);
     ros::Rate loop(30);
