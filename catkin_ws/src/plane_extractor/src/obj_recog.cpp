d9900f3921b70f5da5557b40fab95c60b9321f90
Mark The Magnificent <marco.negrete@ingenieria.unam.edu>
2022-04-28 20:46:17 -0500

    Move mathworks simulations to this repo Add node for clustering

--- catkin_ws/src/plane_extractor/src/obj_recog.cpp
+++ catkin_ws/src/plane_extractor/src/obj_recog.cpp
@@ -3,11 +3,13 @@
 #include "std_msgs/Empty.h"
 #include "sensor_msgs/PointCloud2.h"
 #include "sensor_msgs/Image.h"
+#include "geometry_msgs/Point.h"
 #include "visualization_msgs/Marker.h"
 #include "tf/transform_listener.h"
 #include "pcl_ros/transforms.h"
 
 tf::TransformListener* tf_listener;
+ros::Publisher pub_centroids;
 int z_plane_threshold = 10;
 int dist_threshold    = 10;
 
@@ -53,7 +55,7 @@ cv::Mat color_segmentation(cv::Mat bgr)
     cv::inRange(hsv, cv::Scalar(110, 200, 30), cv::Scalar(130, 255, 255), bin_b);
     cv::inRange(hsv, cv::Scalar( 50, 200, 30), cv::Scalar( 70, 255, 255), bin_g);
     cv::inRange(hsv, cv::Scalar(  0, 200, 30), cv::Scalar( 10, 255, 255), bin_r);
-    cv::inRange(hsv, cv::Scalar( 20, 200, 30), cv::Scalar( 40, 255, 255), bin_y);
+    cv::inRange(hsv, cv::Scalar( 25, 200, 30), cv::Scalar( 35, 255, 255), bin_y);
     cv::bitwise_or(bin_b, bin_g, bin_objects);
     cv::bitwise_or(bin_objects, bin_r, bin_objects);
     cv::bitwise_or(bin_objects, bin_y, bin_objects);
@@ -69,16 +71,36 @@ cv::Mat clusterize(cv::Mat bin, cv::Mat xyz_robot)
         for(int j=0; j < bin.cols; j++)
             if(bin.at<unsigned char>(i,j) != 0)
                 filtered.push_back(xyz_robot.at<cv::Vec3f>(i,j));
-    cv::kmeans(filtered, 5, labels, cv::TermCriteria( cv::TermCriteria::EPS + cv::TermCriteria::COUNT, 10, 1.0),
+    cv::kmeans(filtered, 5, labels, cv::TermCriteria( cv::TermCriteria::EPS + cv::TermCriteria::COUNT, 10, 0.1),
                3, cv::KMEANS_PP_CENTERS, centers);
-    //std::cout << "Found " << centers.rows<< " centroids from  " << filtered.size() << " points" << std::endl;
     return centers;
 }
 
 visualization_msgs::Marker get_object_marker(cv::Mat centroids)
 {
     visualization_msgs::Marker mrk;
-    
+    mrk.header.frame_id = "world";
+    mrk.header.stamp = ros::Time::now();
+    mrk.ns = "obj_centroids";
+    mrk.id = 0;
+    mrk.type = visualization_msgs::Marker::SPHERE_LIST;
+    mrk.action = visualization_msgs::Marker::ADD;
+    mrk.scale.x = 0.10;
+    mrk.scale.y = 0.10;
+    mrk.scale.z = 0.10;
+    mrk.color.a = 1.0;
+    mrk.color.r = 1.0;
+    mrk.color.g = 0.0;
+    mrk.color.b = 0.0;
+    for(int i=0; i < centroids.rows; i++)
+    {
+        geometry_msgs::Point p;
+        p.x = centroids.at<float>(i, 0);
+        p.y = centroids.at<float>(i, 1);
+        p.z = centroids.at<float>(i, 2);
+        mrk.points.push_back(p);
+    }
+    return mrk;
 }
 
 void callback_point_cloud(const sensor_msgs::PointCloud2::ConstPtr& msg)
@@ -99,6 +121,7 @@ void callback_point_cloud(const sensor_msgs::PointCloud2::ConstPtr& msg)
     bgr = get_image_no_background(bgr, xyz_camera, xyz_robot);
     bin = color_segmentation(bgr);
     centroids = clusterize(bin, xyz_robot);
+    pub_centroids.publish(get_object_marker(centroids));
     cv::imshow("Extracted Plane", bgr);
 }
 
@@ -111,6 +134,7 @@ int main(int argc, char** argv)
     ros::init(argc, argv, "obj_recog");
     ros::NodeHandle n;
     ros::Subscriber sub_cloud = n.subscribe("/camera/depth/points", 1, callback_point_cloud);
+    pub_centroids = n.advertise<visualization_msgs::Marker>("/marker_centroids", 10);
     ros::Rate loop(30);
     tf_listener = new tf::TransformListener();
 
