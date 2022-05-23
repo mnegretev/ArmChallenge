473f2b2651fbb6114a51e30249307448e30e08b4
Mark The Magnificent <marco.negrete@ingenieria.unam.edu>
2022-05-23 01:11:03 -0500

    Update matlab files

--- matlab/calculate_trajectory.m
+++ matlab/calculate_trajectory.m
@@ -13,10 +13,10 @@ function msg = calculate_trajectory(Q_goal, sub_current_pose)
     end
     Q = Q';
     
-    msg = rosmessage("trajectory_msgs/JointTrajectory", "DataFormat", "struct");
+    msg = rosmessage('trajectory_msgs/JointTrajectory', 'DataFormat', 'struct');
     msg.JointNames = {'joint_1','joint_2','joint_3','joint_4','joint_5','joint_6','joint_7'};
     for i=1:length(Q)
-        p = rosmessage("trajectory_msgs/JointTrajectoryPoint", "DataFormat", "struct");
+        p = rosmessage("trajectory_msgs/JointTrajectoryPoint", 'DataFormat', 'struct');
         p.Positions = Q(i,:);
         p.TimeFromStart = rosduration(T(i), 'DataFormat', 'struct');
         trjPts(i) = p;
--- matlab/inverse_kinematics.m
+++ matlab/inverse_kinematics.m
@@ -9,10 +9,10 @@ function Q = inverse_kinematics(x,y,z, robot, sub_current_pose)
     pd = [x y z p(4) p(5) p(6)];
     iterations = 0;
     while norm(p - pd) > 0.01 && iterations < 200
-        J = jacobian(robot, Q);
+        J = jacobian(robot, Q');
         err = p - pd;
         err(4:6) = mod(err(4:6) + pi, 2*pi) - pi;
-        Q = mod(q - pinv(J)*err + pi, 2*pi) - pi;
+        Q = mod(Q - pinv(J)*err' + pi, 2*pi) - pi;
         T = getTransform(robot, Q', 'EndEffector_Link', 'base_link');
         rpy = rotm2eul(T(1:3,1:3));
         p  = [T(1,4) T(2,4) T(3,4) rpy(3) rpy(2) rpy(1)];
